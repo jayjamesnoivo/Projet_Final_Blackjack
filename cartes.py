5f23c004e66ad913b46ee1b085b809990c3441f7
Jeremy-James Noivo <jayjamesnoivo7501@hotmail.com>
2025-12-19 12:57:14 -0500

    Update deck logic and team setup instructions

--- cartes.py
+++ cartes.py
@@ -1,14 +1,13 @@
 import random
 
-def creer_paquet():
-    valeurs = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-    couleurs = ["♠", "♥", "♦", "♣"]
-    paquet = []
-    for v in valeurs:
-        for c in couleurs:
-            paquet.append(f"{v}{c}")
-    random.shuffle(paquet)
-    return paquet
+def create_deck():
+    values = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] #10 = face card & 11 = ace
+    deck = []
+    for v in values:
 
-def distribuer_carte(paquet):
-    return paquet.pop()
+       deck.append(f"{v}")
+    random.shuffle(deck)
+    return deck
+
+def deal_cards(deck):
+    return deck.pop()
